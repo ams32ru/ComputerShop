864637fba8fa7e52be535c762bde0026fe4f98e0
ams32ru <ams32ru@mail.ru>
2023-06-03 17:35:58 +0300

    ноутбуки, контролер и сервис

--- controller/NotebooksController.java
+++ controller/NotebooksController.java
@@ -3,19 +3,46 @@ package com.example.computershop.controller;
 import com.example.computershop.enity.monitors.Monitors;
 import com.example.computershop.enity.notebooks.Notebooks;
 import com.example.computershop.repository.NotebooksRepository;
+import com.example.computershop.service.MonitorsService;
+import com.example.computershop.service.NotebooksService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping(value = "/notebooks",produces = "application/json")
 public class NotebooksController {
     @Autowired
-    NotebooksRepository notebooksRepository;
+    NotebooksService notebooksService;
 
-    @PostMapping("/create-notebook")
-    public Notebooks createNotebooks(){
-        return notebooksRepository.save(new Notebooks());
+    @PostMapping("/create-notebooks")
+    public Notebooks createNotebooks() {
+        return notebooksService.createNotebook();
+    }
+
+    @PutMapping("/put-notebooks")
+    public ResponseEntity<Notebooks> setNotebooks(@RequestBody Notebooks notebook) {
+        Notebooks foundNotebook = notebooksService.editNotebooks(notebook);
+        if (foundNotebook == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(foundNotebook);
+    }
+
+    @GetMapping("/get-notebooks")
+    public ResponseEntity<Notebooks> getNotebooksById(@PathVariable long id) {
+        Notebooks notebooks = notebooksService.getNotebooksById(id);
+        if (notebooks == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(notebooks);
+    }
+
+    @GetMapping("get-all-notebooks")
+    public Collection<Notebooks> getAllNotebooks() {
+        return notebooksService.getAllNotebooks();
     }
 }
---
+++ service/NotebooksService.java
@@ -0,0 +1,32 @@
+package com.example.computershop.service;
+
+import com.example.computershop.enity.notebooks.Notebooks;
+import com.example.computershop.repository.NotebooksRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class NotebooksService {
+    private final NotebooksRepository notebooksRepository;
+
+    public NotebooksService(NotebooksRepository notebooksRepository) {
+        this.notebooksRepository = notebooksRepository;
+    }
+
+    public Notebooks createNotebook() {
+        return notebooksRepository.save(new Notebooks());
+    }
+
+    public Notebooks editNotebooks(Notebooks notebook) {
+        return notebooksRepository.save(notebook);
+    }
+
+    public Notebooks getNotebooksById(long id) {
+        return notebooksRepository.findById(id).orElseThrow();
+    }
+
+    public Collection<Notebooks> getAllNotebooks() {
+        return notebooksRepository.findAll();
+    }
+}
